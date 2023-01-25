05673f59bc705e12ea4a8c78ac43eaaf05279aff
ziyoviddinovazizjon@gmail.com <ziyoviddinovazizjon@gmail.com>
2023-01-25 16:17:46 +0300

    1-dars

---
+++ index.js
@@ -0,0 +1,125 @@
+                       // 1-dars Veraybillar
+
+// console.log('Hello Azizbek');
+
+// let text = 'Salom Webbrain.com'
+// console.log(text);
+
+// console.log(text);
+
+// let text;
+// console.log(text);
+
+// let text;
+// console.log(text);
+// text = 'Hello Azizbek'
+// console.log(text);
+
+// let text = 1;
+// console.log(text);
+// text = 'Hello Azizbek'
+// console.log(text)
+
+// let var const veraybillarni dekler qilishlik belgilash uchun kerak bo'ladi
+// let - hotirada kam joy egallaydi.
+// const - o'zgarmas. o'zgartirsak error beradi.
+// let - scop.  var - global
+
+// let num = 1,
+// b = 2,
+// c = 3;
+// console.log(num, b, c);
+
+// let num = 1;
+// let b = 2;
+// let c = 3;
+// console.log(num, b, c);
+
+// let a=b=c=3;
+// console.log(a,b,c);
+
+                        // data type
+
+// data type nima?
+
+// typeof - o'zgaruvchini malumotini chiqazib beradi
+// 'yozilishi' console.log(typeof 1); 
+// console.log(9007199254740992n + 1n);
+
+// bigtic 
+// stringlar = '' "" ``
+// console.log('Welcom to "Azizbek"');
+// console.log("Welcom to 'Azizbek'");
+
+// let name = ' Azizbek ';
+// console.log('Welcom' + name);
+
+// let name = ' Azizbek ';
+// console.log(`Welcom ${name} IT Center`);
+// `` = stringni ichida o'zgaruvchi ishlata olamiz.
+
+                        // Boolean
+
+// boolean 2 ta qiymati bor true folse 
+// true - to'gri
+// folse - noto'gri
+
+                    //    null
+
+// let a;
+// let b = null;
+// console.log(a,b);
+// null = qiymat biriktilmagan degani
+// undafined va null farqi ikkalasi boshqa boshqa data turiga kiradi.
+
+
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// let a;
+// let b = null; `-->` `buni yaxshi tarafi keyinchalik uni ustida amal bajarsak bo'ladi`
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log(a + 1);
+// console.log(b + 1);
+
+
+// let a;
+// let b
+// console.log(b === b); `premitiv`
+// console.log({b} === {b}); `nan premitiv`
+
+
+                        // Symbol
+
+// Symbol = yunik kiy yasab beradi
+// let a = Symbol('text')
+// let b = Symbol('text')
+
+// console.log(a,b);
+
+// console.log(a === b);
+// console.log(a == b);
+
+
+                    // Object
+
+    // Object nima? object na premimitiv data typeler referensiga qarab turib malumotni chaqirish
+// Objectga = array  obj function kiradi
+// Array - container malumotlar joylanadi
+// let array = []; 'arrey'
+// let obj = {}; 'obyect'
+// let func = ()=>{}; 'fankshn'
+//  function name(){}
+// bularning farqi
+// array = index orqali chaqiriladi. array kop malumot oladi
+// obj = kiy orqali chaqiriladi. obj 1ta odamni malumotini oladi
+// functions = malum bir ko'dlarning to'plami
+
+// let arry = ["Azizbek", 20, "Ziyoviddinov"]; 'arrey'
+// let obj = {name:"Azizbek", status:"IT center"}; 'obyect'
+// let fun = ()=>{}; 'fankshn'
+// function name(){}
+
+// console.log(arry[0]);
+// console.log(obj.name);
